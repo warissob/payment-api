3ce6043d10c21ebf6b9d78c74505b28b1d64490d
Wariss Ondo <warissondo@gmail.com>
2026-07-05 22:32:30 +0100

    Mise ajour

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def paiement():
 def solde():
     return "Solde disponible"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
